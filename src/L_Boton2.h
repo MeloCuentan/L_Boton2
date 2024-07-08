0cd3cefe24dc3c084dcd19f3b86f358820f1949e
Luiti_LP <89021748+MeloCuentan@users.noreply.github.com>
2024-07-08 23:19:18 +0100

    Update L_Boton2.h

--- src/L_Boton2.h
+++ src/L_Boton2.h
@@ -21,19 +21,19 @@ class Boton {
     const uint8_t _PULSO_1 = 1;
     const uint8_t _PULSO_LARGO = 8;
 
+    bool _estadoAnteriorBoton;
+    bool _variasPulsacionesActivado;
     uint8_t _botonPin;
+    uint8_t _accionBoton;
+    uint8_t _estadoActual;
+    uint8_t _contadorPulsaciones;
+    uint8_t _maxPulsaciones;
     uint32_t _tiempoPulsoLargo;
     uint32_t _intervaloPulsaciones;
     uint32_t _tiempoPresionado;
     uint32_t _tiempoUltimaPulsacion;
     uint32_t _tiempoDebounce;
     uint32_t _intervaloDebounce = 50;
-    bool _estadoAnteriorBoton;
-    uint8_t _accionBoton;
-    uint8_t _estadoActual;
-    uint8_t _contadorPulsaciones;
-    uint8_t _maxPulsaciones;
-    bool _variasPulsacionesActivado;
 };
 
 #endif
